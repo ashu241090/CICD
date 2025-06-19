80968757da5f8ff4dd30e14bf92271bfec487269
ashu241090 <58809803+ashu241090@users.noreply.github.com>
2025-06-19 11:53:27 +0530

    Update AssemblyInfo.cs

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -1,1 +1,19 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("GITHUB-CICD")]
+[assembly: AssemblyDescription("CI/CD demo project")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("YourCompanyName")]
+[assembly: AssemblyProduct("GITHUB-CICD App")] 
+[assembly: AssemblyCopyright("Copyright © 2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("d7b6e6f4-99e1-4f9e-a1f6-123456789abc")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
 
